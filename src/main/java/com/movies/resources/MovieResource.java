4684ca257b79bad6ac16d1d52254f99adc58f075
yadoshi <yagodfernandes@gmail.com>
2023-12-11 15:29:56 -0300

    Metodo getByid

--- src/main/java/com/movies/resources/MovieResource.java
+++ src/main/java/com/movies/resources/MovieResource.java
@@ -3,14 +3,13 @@ package com.movies.resources;
 import com.movies.entities.Movie;
 import com.movies.repository.MovieRepository;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.List;
+
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
 
 @Path("/movies")
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,5 +23,14 @@ public class MovieResource {
     public Response getAll(){
         List<Movie> movies = movieRepository.listAll();
         return Response.ok(movies).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getById(@PathParam("id") Long id){
+        return movieRepository
+                .findByIdOptional(id)
+                .map(movie -> Response.ok(movie).build())
+                .orElse(Response.status(NOT_FOUND).build());
     }
 }
