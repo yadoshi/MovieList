9e3121a604751fbb92e8f3feda54a554bee8401f
yadoshi <yagodfernandes@gmail.com>
2023-12-12 11:02:07 -0300

    create,delete e update movies

--- src/main/java/com/movies/entities/Movie.java
+++ src/main/java/com/movies/entities/Movie.java
@@ -14,4 +14,44 @@ public class Movie {
     private String description;
     private String director;
     private String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
--- src/main/java/com/movies/resources/MovieResource.java
+++ src/main/java/com/movies/resources/MovieResource.java
@@ -7,7 +7,11 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
 
@@ -18,10 +22,12 @@ public class MovieResource {
 
     @Inject
     MovieRepository movieRepository;
+    public static List<Movie> movies = new ArrayList<>();
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getAll(){
-        List<Movie> movies = movieRepository.listAll();
+        var movieList = movieRepository.listAll();
         return Response.ok(movies).build();
     }
 
@@ -30,7 +36,7 @@ public class MovieResource {
     public Response getById(@PathParam("id") Long id){
         return movieRepository
                 .findByIdOptional(id)
-                .map(movie -> Response.ok(movie).build())
+                .map(movie -> Response.ok(movies).build())
                 .orElse(Response.status(NOT_FOUND).build());
     }
 
@@ -45,9 +51,58 @@ public class MovieResource {
     }
 
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/size")
+    public Integer countMovies(){
+        return movies.size();
+    }
+
+    @GET
     @Path("country/{country}")
     public Response getByCountry(@PathParam("country") String country){
         List<Movie> movies = movieRepository.findByCountry(country);
         return Response.ok(movies).build();
     }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createMovie(Movie newMovie){
+        movies.add(newMovie);
+        return Response.ok(movies).build();
+    }
+
+    @PUT
+    @Path("{id}/{title}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateMovie(
+            @PathParam("id") Long id,
+            @PathParam("title") String title){
+        movies = movies.stream().map(movie -> {
+            if (movie.getId().equals(id)){
+                movie.setTitle(title);
+            }
+            return movie;
+        }).collect(Collectors.toList());
+        return Response.ok(movies).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response deleteMovie(
+            @PathParam("id") Long id){
+        Optional<Movie> movieToDelete = movies.stream().filter(movie -> movie.getId().equals(id))
+                .findFirst();
+        boolean removed = false;
+        if (movieToDelete.isPresent()){
+            removed = movies.remove(movieToDelete.get());
+        }
+        if (removed){
+            return Response.noContent().build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
 }
