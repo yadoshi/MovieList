a150c5b07da917d38da29b135f7dfb9024f5d9e0
yadoshi <yagodfernandes@gmail.com>
2023-12-14 17:53:32 -0300

    openAPI

--- src/main/java/com/movies/MovieApplication.java
+++ src/main/java/com/movies/MovieApplication.java
@@ -1,4 +1,24 @@
 package com.movies;
 
-public class MovieApplication {
+import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Movie APIs",
+                description = "Movie Application",
+                version = "1.0.0",
+                license = @License(
+                        name = "MIT",
+                        url = "https://localhost:8080"
+                )
+        ),
+        tags = {
+                @Tag(name = "movies", description = "Movies")
+        }
+)
+public class MovieApplication extends Application {
 }
